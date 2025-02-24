17bdf78212a7ffc5d11184c8afd620be02cc90d3
Fabio Kuriki <fhcesare@stefanini.com>
2025-02-24 11:28:56 -0300

    Adicionando a responsividade no sidebar

--- src/app/componentes/sidebar/sidebar.component.ts
+++ src/app/componentes/sidebar/sidebar.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
-
+import {MediaMatcher} from '@angular/cdk/layout';
+import {Component, OnDestroy, inject, signal} from '@angular/core';
+import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
+/** @title Responsive sidenav */
 @Component({
   selector: 'app-sidebar',
-  imports: [MatSidenavModule, MatButtonModule, MatIconModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
 })
-export class SidebarComponent {
-  mostrarSidebar = false;
+export class SidebarComponent implements OnDestroy {
+  protected readonly fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  protected readonly isMobile = signal(true);
+
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
+
+  constructor() {
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
 }
