b9fddcc8c1dada6cbed2e319acfedc5a675c4df4
Fabio Kuriki <fhcesare@stefanini.com>
2025-02-25 10:51:35 -0300

    Adição de dados na tabela

--- src/app/paginas/ordem-fornecimento/ordem-fornecimento.component.ts
+++ src/app/paginas/ordem-fornecimento/ordem-fornecimento.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-ordem-fornecimento',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ordem-fornecimento.component.html',
   styleUrl: './ordem-fornecimento.component.css'
 })
 export class OrdemFornecimentoComponent {
+  ordemFornecimentos = [
+    {
+    id: '1',
+    colaborador: 'Fabio',
+    descricao: 'Instalação de Software',
+    status: 'Iniciada',
+    criacao: '20/02/25',
+    atualizacao: '25/02/25'
+    },
+    {
+      id: '2',
+      colaborador: 'Shirley',
+      descricao: 'Manutenção da impressora',
+      status: 'Iniciada',
+      criacao: '15/01/25',
+      atualizacao: '23/02/25'
+      },
+  ]
+
+  id = "";
+  colaborador = "";
+  descricao = "";
+  status = "";
+  criacao = "";
+  adicionarOf(){
+    this.ordemFornecimentos.push({id: this.id,
+                                  colaborador: this.colaborador,
+                                  descricao: this.descricao,
+                                  status: this.status,
+                                  criacao: "25/02/25",
+                                  atualizacao: ""})
+  }
 }
