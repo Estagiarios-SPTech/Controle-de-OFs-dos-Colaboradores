9068615295cf02beba56fca4a2e3adeec4645fe6
Shirley Ferreira Lima <sflima2@stefanini.com>
2025-03-06 14:05:25 -0300

    componentes do angular material no login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
