927afb17f9f8fc544143d5c65ce4d0a0085b713b
Fabio Kuriki <fhcesare@stefanini.com>
2025-03-10 12:53:51 -0300

    Conectando sidebar as demais paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { UsuariosComponent } from './paginas/usuarios/usuarios.component';
+import { OrdemFornecimentoComponent } from './paginas/ordem-fornecimento/ordem-fornecimento.component';
 
 export const routes: Routes = [
-    {path: 'usuarios', component: UsuariosComponent}
+    {path: '', component: UsuariosComponent},
+    {path: 'usuarios', component: UsuariosComponent},
+    {path: 'ordemServico', component: OrdemFornecimentoComponent}
 ];
