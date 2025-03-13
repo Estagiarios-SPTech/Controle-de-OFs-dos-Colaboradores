45e9089fe9c83ce7a03cd6ca1cf77ab88e11a28d
Shirley Ferreira Lima <sflima2@stefanini.com>
2025-03-13 12:53:23 -0300

    graficos melhorados

--- src/app/componentes/ordens-de-servico/ordens-de-servico.component.ts
+++ src/app/componentes/ordens-de-servico/ordens-de-servico.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import {ChangeDetectionStrategy} from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import { Chart, ChartConfiguration, ChartData, ChartType, registerables} from 'chart.js';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Chart, ChartConfiguration, ChartData, ChartType, registerables } from 'chart.js';
 
 
 @Component({
@@ -12,68 +12,110 @@ import { Chart, ChartConfiguration, ChartData, ChartType, registerables} from 'c
 })
 export class OrdensDeServicoComponent implements OnInit {
 
-   chart: Chart | undefined; 
-   chartBar: Chart | undefined;
+  chart: Chart | undefined;
+  chartBar: Chart | undefined;
 
-   ngOnInit() {
+  ngOnInit() {
     const ctx = document.getElementById('chart-pie') as HTMLCanvasElement;
     const ctx2 = document.getElementById('chart-bars') as HTMLCanvasElement;
-    
+
     OrdensDeServicoComponent.constructor(); {
       // Register all required Chart.js components globally
       Chart.register(...registerables);
     }
-  
-  this.chart = new Chart(ctx, {
-    type: 'pie',
-     data: {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-      ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    },
-  });
 
-  this.chartBar = new Chart (ctx2, {
-    type: 'bar',
-    data: {
-      labels: ['Janeiro','Fevereiro'],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 205, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(201, 203, 207, 0.2)'
+    this.chart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: [
+          'Pendente de Cadastramento',
+          'Validada',
+          'Iniciada'
         ],
-        borderColor: [
-          'rgb(255, 99, 132)',
-          'rgb(255, 159, 64)',
-          'rgb(255, 205, 86)',
-          'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)',
-          'rgb(201, 203, 207)'
-        ],
-        borderWidth: 1
-      }]
-    }
-  })
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(168, 0, 64)',
+            'rgb(78, 138, 0)',
+            'rgb(231, 162, 1)'
+          ],
+          borderWidth: 1,
+          borderColor: '#333', 
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              font: {
+                size: 14,
+                weight: 'normal',
+                family: 'Helvetica',
+              },
+              usePointStyle: true,
+              color: '#333',
+              boxWidth: 10,
+              boxHeight: 10,
+              padding: 10,
+            }
+          }
+        }
+      }
+    });
 
-}
+    this.chartBar = new Chart(ctx2, {
+      type: 'bar',
+      data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+        datasets: [{
+          label: 'Ordens de Fornecimento',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(76, 0, 255, 0.2)',
+            'rgba(76, 0, 255, 0.2)',
+            'rgba(76, 0, 255, 0.2)',
+            'rgba(76, 0, 255, 0.2)',
+            'rgba(76, 0, 255, 0.2)',
+            'rgba(76, 0, 255, 0.2)'
+
+          ],
+          borderColor: [
+            'rgb(99, 33, 255)',
+            'rgb(99, 33, 255)',
+            'rgb(99, 33, 255)',
+            'rgb(99, 33, 255)',
+            'rgb(99, 33, 255)',
+            'rgb(99, 33, 255)'
+          ],
+          borderWidth: 1
+        }]
+      }, 
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              font: {
+                size: 14,
+                weight: 'normal',
+                family: 'Helvetica',
+              },
+              borderRadius: 2,
+              color: '#333',
+              boxWidth: 10,
+              boxHeight: 10,
+              padding: 10,
+            }
+          }
+        }
+      }
+    })
+
+  }
 }
