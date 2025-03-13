caac542ebd4e684aead016e4406a0758c0f2c10b
Fabio Kuriki <fhcesare@stefanini.com>
2025-03-13 11:27:20 -0300

    renomeando modules para paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
-import { OrdemFornecimentoComponent } from './modulos/ordem-fornecimento/ordem-fornecimento.component';
-import { HomeComponent } from './modulos/home/home.component';
+import { UsuariosComponent } from './paginas/usuarios/usuarios.component';
+import { OrdemFornecimentoComponent } from './paginas/ordem-fornecimento/ordem-fornecimento.component';
+import { HomeComponent } from './paginas/home/home.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
