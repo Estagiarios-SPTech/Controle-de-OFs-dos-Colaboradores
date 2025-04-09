6ea6b1df0f593cc7a7e21d8a98a38d7dffa612d3
Fabio Kuriki <fhcesare@stefanini.com>
2025-04-09 13:48:58 -0300

    Conserto de alguns routerLinks

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,15 +1,13 @@
 import {Component, Input, input} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [MatCardModule, RouterLink],
+  imports: [MatCardModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
   @Input() titulo: string = "";
   @Input() img: string = "";
-  @Input() rota: string = "";
 }
--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../componentes/card/card.component';
 import { ConsultarUsuarioComponent } from '../../componentes/consultar-usuario/consultar-usuario.component';
 import { GraficoEmpilhadoComponent } from '../../componentes/grafico-empilhado/grafico-empilhado.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent, ConsultarUsuarioComponent, GraficoEmpilhadoComponent],
+  imports: [CardComponent, ConsultarUsuarioComponent, GraficoEmpilhadoComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
