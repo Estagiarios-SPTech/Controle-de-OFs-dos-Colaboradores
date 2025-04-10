f0a352f8b6d85db1427cdf11e36e600ecc100aae
Shirley Ferreira Lima <sflima2@stefanini.com>
2025-04-10 14:56:50 -0300

    Alterações do cadastro do employeer

--- src/app/componentes/cadastrar-usuario/cadastrar-usuario.component.ts
+++ src/app/componentes/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -67,93 +67,51 @@ export class CadastrarUsuarioComponent {
   user: User = new User();
   employee: Employee = new Employee();
 
-  teste(texto: string): void {
-    console.log(texto);
+  // teste(texto: string): void {
+  //   console.log(texto);
+  // }
+
+  cadastrar(): void {
+    this.usuarioService.signUp(this.user).subscribe({
+      next: (usuarioCriado) => {
+        console.log("cadastro feito com sucesso" + this.user);
+        if (this.user.role === 'Colaborador') {
+          const managerObj = this.managers.find(m => m.name === this.selectManager);
+          if (managerObj) {
+            this.employee.manager = managerObj;
+          }
+
+          const rtObj = this.rts.find(r => r.name === this.selectRt);
+          if (rtObj) {
+            this.employee.rt = rtObj;
+          }
+        }
+        this.employee.employee = usuarioCriado;
+        this.employee.status = "Disponivel";
+
+          this.employeeService.cadastrarEmployee(this.employee).subscribe({
+            next: (employeeCriado) => {
+              console.log('Employee cadastrado com sucesso:', employeeCriado);
+              this.resetForm();
+            },
+            error: (error) => {
+              console.error('Erro ao cadastrar employee:', error);
+            }
+          
+        });
+      },
+      error: (error) => {
+        console.error('Erro ao cadastrar usuário:', error);
+      }
+    });
+     {
+          
+      // const rtSelecionado = this.rts.find(r => r.name === this.selectRt);
+      // if (rtSelecionado && rtSelecionado.id !== undefined) {
+      //   rtId = rtSelecionado.id;
+      // }    
+    }
   }
-
-  // newCadastro(user: User): void {
-  //   console.log(user);
-  //   this.usuarioService.signUp(user).subscribe(retorno => console.log(retorno));
-  //   if (this.cargo === 'Colaborador') {
-      
-      
-  //     // let rtId: number | undefined;
-  //     let theManager: User;
-      
-  //     // const rtSelecionado = this.rts.find(r => r.name === this.selectRt);
-  //     // if (rtSelecionado && rtSelecionado.id !== undefined) {
-  //     //   rtId = rtSelecionado.id;
-  //     // }
-      
-  //     const managerSelecionado = this.managers.find(m => m.name === this.selectManager);
-  //     if (managerSelecionado && managerSelecionado !== undefined) {
-  //     theManager = managerSelecionado;
-
-  //     }
-
-  //     this.employee.id = 0;
-  //     this.employee.employee = user;
-  //     this.employee.manager = ;
-
-      
-     
-  //     this.employeeService.cadastrarEmployee(this.employee)
-  //   }
-  // }
-
-  // cadastrar(): void {
-  //   const user: User = {
-  //     id: 0,
-  //     name: this.nome,
-  //     email: this.email,
-  //     role: this.cargo
-  //   };
-
-  //   console.log(this.variavel);
-  //   // primeiro cadastra o usuário, depois cadastra o funcionário
-  //   this.usuarioService.signUp(user).pipe(
-  //     switchMap(usuarioCriado => {
-  //       console.log(this.variavel);
-  //       let managerId: number | null = null;
-  //       let rtId: number | null = null;
-
-  //       if (this.cargo === 'Colaborador') {
-  //         if (this.selectManager) {
-  //           const managerSelecionado = this.managers.find(m => m.name === this.selectManager);
-  //           if (managerSelecionado && managerSelecionado !== undefined) {
-  //             managerId = managerSelecionado.id;
-  //           }
-  //         }
-
-  //         if (this.selectRt) {
-  //           const rtSelecionado = this.rts.find(r => r.name === this.selectRt);
-  //           if (rtSelecionado && rtSelecionado.id !== undefined) {
-  //             rtId = rtSelecionado.id;
-  //           }
-  //         }
-  //       }
-
-  //         const employeeData = new Employee();
-  //         employeeData.id = 0;  
-  //         employeeData.fk_user = usuarioCriado.id;
-  //         employeeData.fk_manager = managerId;
-  //         employeeData.fk_rt = rtId;
-  //         employeeData.status = 'Disponível'; // padrão
-
-  //         console.log(employeeData)
-
-  //       return this.employeeService.cadastrarEmployee(employeeData);
-  //     })
-  //   ).subscribe({
-  //     next: (employeeResponse) => {
-  //       console.log('Usuário e funcionário cadastrados com sucesso', employeeResponse);
-  //       this.resetForm();
-  //     },
-  //     error: (error) => {
-  //       console.error('Erro ao cadastrar usuário ou funcionário', error);
-  //     }
-  //   });
-  // }
 
   private resetForm(): void {
     this.nome = '';
