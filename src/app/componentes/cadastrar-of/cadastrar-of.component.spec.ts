ba23a17c0353a060646879a5bb309c8502a1aead
Fabio Kuriki <fhcesare@stefanini.com>
2025-06-05 14:56:44 -0300

    component cadastroOF coberto

--- src/app/componentes/cadastrar-of/cadastrar-of.component.spec.ts
+++ src/app/componentes/cadastrar-of/cadastrar-of.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CadastrarOFComponent } from './cadastrar-of.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { OrdemFornecimento } from '../../model/ordemFornecimento';
+import { Employee } from '../../model/Employee';
+import { of } from 'rxjs';
 
 describe('CadastrarOFComponent', () => {
   let component: CadastrarOFComponent;
@@ -8,7 +13,7 @@ describe('CadastrarOFComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CadastrarOFComponent]
+      providers: [provideHttpClient(), provideAnimationsAsync()]
     })
     .compileComponents();
 
@@ -20,4 +25,22 @@ describe('CadastrarOFComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deve cadastrar', () => {
+    var mockEmployee: Employee = new Employee()
+    var mockOF: OrdemFornecimento = {
+      codigo: 1,
+      collaborator: mockEmployee,
+      description: "",
+      status: "",
+      created_at: "",
+      updated_at: "",
+    };
+    spyOn(component.realizouCadastro, 'emit')
+    spyOn(component.ofService, 'verificarCadastro').and.returnValue(of(mockOF))
+
+    component.cadastrar()
+
+    expect(component.realizouCadastro.emit).toHaveBeenCalledTimes(1)
+  })
 });
--- src/app/componentes/cadastrar-usuario/cadastrar-usuario.component.spec.ts
+++ src/app/componentes/cadastrar-usuario/cadastrar-usuario.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CadastrarUsuarioComponent } from './cadastrar-usuario.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { User } from '../../model/User';
+import { of, throwError } from 'rxjs';
 
-describe('CadastrarUsuarioComponent', () => {
+fdescribe('CadastrarUsuarioComponent', () => {
   let component: CadastrarUsuarioComponent;
   let fixture: ComponentFixture<CadastrarUsuarioComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CadastrarUsuarioComponent]
+      providers: [provideHttpClient(), provideAnimationsAsync()]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CadastrarUsuarioComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deve carregar os gerentes', () => {
+    var gerentes: User[] = []
+
+    spyOn(component.usuarioService, 'selectManager').and.returnValue(of(gerentes))
+
+    component.loadManagers()
+  
+    expect(component.managers).toBe(gerentes)
+  });
+
+  it('Deve gerar erro ao carregar os gerentes', () => {
+    spyOn(console, 'error');
+    spyOn(component.usuarioService, 'selectManager').and.returnValue(throwError(() => new Error));
+    
+    component.loadManagers();
+    
+    expect(console.error).toHaveBeenCalledWith('Erro ao carregar dados', jasmine.any(Error));
+  });
+
+  it('Deve carregar os RTs', () => {
+    var rts: User[] = []
+
+    spyOn(component.usuarioService, 'selectRt').and.returnValue(of(rts))
+
+    component.loadRts()
+  
+    expect(component.rts).toBe(rts)
+  });
+
+  it('Deve gerar erro ao carregar os RTs', () => {
+    spyOn(console, 'error');
+    spyOn(component.usuarioService, 'selectRt').and.returnValue(throwError(() => new Error));
+    
+    component.loadRts();
+    
+    expect(console.error).toHaveBeenCalledWith('Erro ao carregar dados', jasmine.any(Error));
   });
 });
