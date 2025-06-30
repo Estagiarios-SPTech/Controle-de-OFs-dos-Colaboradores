f3c8592a75c8617f19bbdd46893fda79616ab1d3
Fabio Kuriki <fhcesare@stefanini.com>
2025-06-30 10:56:06 -0300

    Mudança na exbição da tabela de usuários com base no role

--- src/app/paginas/usuarios/usuarios.component.ts
+++ src/app/paginas/usuarios/usuarios.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CadastrarUsuarioComponent } from '../../componentes/cadastrar-usuario/cadastrar-usuario.component';
 import { ColaboradoresComponent } from "../../componentes/colaboradores/colaboradores.component";
+import { ConsultarUsuarioComponent } from "../../componentes/consultar-usuario/consultar-usuario.component";
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [CadastrarUsuarioComponent, ColaboradoresComponent],
+  imports: [CadastrarUsuarioComponent, ColaboradoresComponent,
+    ConsultarUsuarioComponent, CommonModule],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
 export class UsuariosComponent {
-
+  constructor(public auth:AuthService){}
 }
